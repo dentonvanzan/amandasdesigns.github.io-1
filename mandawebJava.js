9c099a40599d5e245bb7b440d598db9da945e8d3
dentonvanzan <54280093+dentonvanzan@users.noreply.github.com>
2019-12-13 11:35:10 -0500

    Update email

--- mandawebJava.js
+++ mandawebJava.js
@@ -29,3 +29,6 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+function sendEmail() {
+    window.location.assign("mailto:kevintaylor.it@outlook.com");
+}
